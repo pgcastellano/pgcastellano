3e21ed6e868b9b2a8b4920664b6226850c40fb3d
pgcastellano <pilargomezcastellano@gmail.com>
2022-10-30 17:33:34 -0300

    todo

--- TRABAJOS PRACTICOS/Trabajo practico integrador 2 JS/comprartickets.js
+++ TRABAJOS PRACTICOS/Trabajo practico integrador 2 JS/comprartickets.js
@@ -1,14 +1,14 @@
 // CALCULADORA PARA TOTAL A PAGAR POR TICKETS EN FORM
 
 
-    function total (categoria, cantidad) {
-        // debugger;
-        cantidad = document.getElementById("cantidad").value;
-        categoria = document.getElementById("categoria").value;
+function total(categoria, cantidad) {
+    // debugger;
+    cantidad = document.getElementById("cantidad").value;
+    categoria = document.getElementById("categoria").value;
 
-        if (categoria === "Estudiante") document.getElementById('montoTotal').innerHTML = `$${(cantidad * 40)}`; 
-        else if (categoria === "Trainee") document.getElementById('montoTotal').innerHTML = `$${(cantidad * 100)}`;
-        else document.getElementById('montoTotal').innerHTML = `$${(cantidad * 170)}`;
-    }
+    if (categoria === "Estudiante") document.getElementById('montoTotal').innerHTML = `$${(cantidad * 40)}`;
+    else if (categoria === "Trainee") document.getElementById('montoTotal').innerHTML = `$${(cantidad * 100)}`;
+    else document.getElementById('montoTotal').innerHTML = `$${(cantidad * 170)}`;
+}
 
 
